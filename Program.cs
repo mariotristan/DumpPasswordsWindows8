3b4cc38647004b2ad9e4a3d12b2e046c3a54c747
Mario Tristan <mariotristan@gmail.com>
2013-09-13 23:36:43 -0500

    Se agrega save to file

--- Program.cs
+++ Program.cs
@@ -1,17 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
 namespace DumpCredentials
 {
     class Program
+      
     {
+        static string mydocpath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        static StringBuilder sb = new StringBuilder();
         static void DumpCredentials(Windows.Security.Credentials.PasswordCredential cred)
         {
+          
             Console.WriteLine("Resource: {0}", cred.Resource);
             Console.WriteLine("UserName: {0}", cred.UserName);
             Console.WriteLine("Password: {0}", cred.Password);
+sb.Append(" Resource: ");
+sb.Append(cred.Resource);
+         sb.Append(" UserName: ");
+         sb.Append( cred.UserName);
+         sb.Append(" Password: " );
+         sb.Append( cred.Password);
+            sb.AppendLine();
+            sb.AppendLine();
         }
         static void Main(string[] args)
         {
@@ -22,6 +35,12 @@ namespace DumpCredentials
                 cred.RetrievePassword();
                 DumpCredentials(cred);
             }
+            using (StreamWriter outfile = new StreamWriter(mydocpath + @"\PassContra.txt"))
+            {
+                outfile.Write(sb.ToString());
+            }
+
+            Console.ReadLine();
 
         }
     }
